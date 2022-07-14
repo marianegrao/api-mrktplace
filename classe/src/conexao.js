62fe3df63c9769db6c5b932e22ea59757bacc29f
MaricaClara <claranegrao11@gmail.com>
2022-07-14 16:41:15 -0300

    Conexão com o banco de dados no heroku criada

--- classe/src/conexao.js
+++ classe/src/conexao.js
@@ -1,10 +1,14 @@
 const knex = require("knex")({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'market_cubos'
+        host: 'ec2-3-219-52-220.compute-1.amazonaws.com',
+        user: 'flwlvdigdpwabv',
+        password: '7ed8c01ffb5cdafb8f156421cf3425cfed092b16a21deccc0b3a49b38109793f',
+        database: 'd2drag2i50t10h',
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 })
 
